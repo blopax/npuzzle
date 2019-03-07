05af0eee113322f08d908cf2a56d5d3be56c611a
Pablo CLEMENT <pclement@e2r6p2.42.fr>
2019-03-07 15:22:21 +0100

    Patern design of utils

---
+++ utils.py
@@ -0,0 +1,25 @@
+
+
+def create_goal(n) ->list:
+    """
+    Returns as a list the goal to reach for a n-puzzle of size n in a Snale style"
+    :param int n: size of puzzle
+    :return list: list of size n^2 with the solution
+    """
+
+
+def action(puzzle, i) ->list:
+    """
+    Returns the state of the puzzle after swapping tile number i. Returns error if tile can not be swapped.
+    :param list puzzle:
+    :param int i:
+    :return list:
+    """
+
+
+def print_puzzle(puzzle) ->None:
+    """
+
+    :param list puzzle:
+    :return:
+    """
