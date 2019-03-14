a5deefdfbc6abf35a8e3cdc2bdbd44d7fc0e4c34
Pablo <pablo87clement@gmail.com>
2019-03-14 09:25:01 +0100

    Split sort_queue and verbose print function from algo

--- algo.py
+++ algo.py
@@ -2,60 +2,85 @@ import node
 import utils
 
 
-size = 5
+size = 3
 goal = utils.create_goal(size)
 
 
-def search_algo(initial_node, mode) -> None:
+def search_algo(initial_node, mode, verbose=False) -> None:
     """
     A* algorithm to find an optimal solution to problem node.
     :param Node initial_node: Problem node with initial conditions.
     :param str mode: Mode of algorithm. Has to be in ["a_star", "greedy", "uniform_cost"]
+    :param bool verbose: verbose mode will print more information during search
     :return: None
     """
     if size % 2 == 1 and utils.puzzle_has_snail_solution(initial_node.state) is False:
-        return finished(None, 0, 0)
-    time_complexity = 1
-    space_complexity = 1
-    nodes_queue = [initial_node]
-    explored_states = [initial_node.state]
+        return finished(None, 0, 0, verbose=verbose)
+    time_complexity, space_complexity = 1, 1
+    nodes_queue, explored_states = [initial_node], [initial_node.state]
     if initial_node.finished is True:
-        return finished(initial_node, time_complexity, space_complexity)
+        return finished(initial_node, time_complexity, space_complexity, verbose=verbose)
     while nodes_queue:
-        if mode == "a_star":
-            sorted_queue = sorted(nodes_queue, key=lambda x: x.evaluation)  # selection du best node
-        elif mode == "greedy":
-            sorted_queue = sorted(nodes_queue, key=lambda x: x.heuristic)  # selection du best node
-        elif mode == "uniform_cost":
-            sorted_queue = sorted(nodes_queue, key=lambda x: x.cost)  # selection du best node
-        else:
-            return finished(None, 0, 0, True)
+        sorted_queue = sort_queue(nodes_queue, mode)
+        if not sorted_queue:
+            return finished(None, 0, 0, error=True)
         best_node = sorted_queue[0]
         for action in best_node.possible_actions:
             state = utils.action(best_node.state, action)
             if state not in explored_states:
                 new_node = node.Node(best_node, action, state)
                 time_complexity += 1
-                if time_complexity % 10000 == 0:
-                    print("time complexity = {}, space complexity = {}, queue = {}".format(
-                        time_complexity, space_complexity, len(sorted_queue)))
-                    print(utils.puzzle_formatted_str(new_node.state))
                 nodes_queue.append(new_node)
                 explored_states.append(new_node.state)
                 space_complexity = max(space_complexity, len(explored_states))
+                if verbose:
+                    verbose_print(time_complexity, space_complexity, sorted_queue, new_node)
                 if new_node.finished is True:
-                    return finished(new_node, time_complexity, space_complexity)
+                    return finished(new_node, time_complexity, space_complexity, verbose=verbose)
         nodes_queue.remove(best_node)
-    return finished(None, 0, 0)
+    return finished(None, 0, 0, verbose=verbose)
 
 
-def finished(finish_node, time_complexity, space_complexity, error=False) -> None:
+def sort_queue(queue, mode) -> list:
+    """
+    Queue is sorted according to mode
+    :param list queue:
+    :param str mode:
+    :return list sorted_queue:
+    """
+    sorted_queue = None
+    if mode == "a_star":
+        sorted_queue = sorted(queue, key=lambda x: x.evaluation)
+    elif mode == "greedy":
+        sorted_queue = sorted(queue, key=lambda x: x.heuristic)
+    elif mode == "uniform_cost":
+        sorted_queue = sorted(queue, key=lambda x: x.cost)
+    return sorted_queue
+
+
+def verbose_print(time_complexity, space_complexity, sorted_queue, new_node) -> None:
+    """
+    Print information during the search.
+    :param int time_complexity:
+    :param int space_complexity:
+    :param list sorted_queue:
+    :param Node new_node:
+    :return None:
+    """
+    if time_complexity % 10000 == 0:
+        print("time complexity = {}, space complexity = {}, queue = {}".format(
+            time_complexity, space_complexity, len(sorted_queue)))
+        print(utils.puzzle_formatted_str(new_node.state))
+
+
+def finished(finish_node, time_complexity, space_complexity, error=False, verbose=False) -> None:
     """
     Function that print solution when found.
     :param Node finish_node: Solution leaf.
     :param int time_complexity: number of nodes that have been created.
     :param int space_complexity: max concurential nodes in memory.
     :param bool error: if there is an error in the parameters given to search algo.
+    :param bool verbose: verbose mode will print more information during search
     :return: None
     """
     if error is True:
@@ -75,8 +100,9 @@ def finished(finish_node, time_complexity, space_complexity, error=False) -> Non
         The steps to solve it are the following {}""".format(
             len(solution_list) - 1, time_complexity, space_complexity, [item.moved_tile for item in solution_list[1:]]))
 
-        # for item in solution_list:
-        #     print(utils.puzzle_formatted_str(item.state))
+        if verbose:
+            for item in solution_list:
+                print(utils.puzzle_formatted_str(item.state))
 
 
 if __name__ == "__main__":
@@ -94,5 +120,5 @@ if __name__ == "__main__":
     # initial_node = node.Node(None, None, utils.create_goal(3), utils.create_goal(3))
     # print(initial_node.__str__())
     # search_algo(init_node, mode="a_star")
-    search_algo(init_node, mode="greedy")
+    search_algo(init_node, mode="a_star")
     # search_algo(init_node, mode="uniform_cost")
