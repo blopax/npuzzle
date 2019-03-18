0cf8ba2c8a329a5a32fcd4f5d86bb84d4e50e2b3
Nicolas VERGNAC <nvergnac@e2r4p4.42.fr>
2019-03-18 19:09:28 +0100

    Handling file problems - setting toggle argument system for time/visu/verbose

--- npuzzle.py
+++ npuzzle.py
@@ -3,6 +3,7 @@ import parser
 import algo
 import node
 import utils
+import os
 
 
 
@@ -16,12 +17,9 @@ def get_args():
     parser.add_argument("-a", "--algorithm", type=str, default="a_star",
                         choices=["a_star", "ida_star", "greedy", "uniform_cost"],
                         help="Choose the algorithm to be used.\n")
-    parser.add_argument("-v", "--verbosity", type=int, default=False, choices={True, False},
-                        help="Verbose mode.\n")
-    parser.add_argument("-t", "--time", type=str, default=False, choices={False, True},
-                        help="Show the time needed to resolve the npuzzle.\n")
-    parser.add_argument("-visu", "--visual", type=bool, default=False, choices={False, True},
-                        help="Toggle Visual mode.\n")
+    parser.add_argument("-v", "--verbosity", action="store_true", help="Toggle verbose mode.\n")
+    parser.add_argument("-t", "--time", action="store_true", help="Show the time needed to resolve the npuzzle.\n")
+    parser.add_argument("-visu", "--visual", action="store_true", help="Toggle Visual mode.\n")
     parser.add_argument("-visumode", "--visual_mode", type=str, default="fight", choices={"fight", "solution"},
                         help="Define the type of visual.\n")
     parser.add_argument("-p", "--puzzle", type=str, default="snail", choices={"snail", "classic"},
@@ -33,14 +31,13 @@ def get_args():
 if __name__ == "__main__":
     args = get_args()
     algo_info = utils.create_info_algo(args)
-    parser.check_file(algo_info, args.file)
+    if os.path.isfile(args.file):
+        parser.check_file(algo_info, args.file)
+    else:
+        algo_info["error"] = "{0} is not a valid file".format(args.file)
     if algo_info["error"] == '':
         algo_info["goal"] = utils.create_goal(algo_info["board_size"], algo_info["goal_kind"])
         init_node = node.Node(None, None, algo_info["puzzle"], algo_info["heuristic"], algo_info["board_size"], algo_info["goal"])
         algo.algo(init_node, algo_info)
     else:
         print(algo_info["error"])
-
-
-# parser add relevant info and feed info dictionary (where +, maybe create util functions with default)
-# + create initial Node
--- parser.py
+++ parser.py
@@ -15,7 +15,13 @@ def strip_comment(line):
     return line.strip()
 
 
-def clean_data(algo_info, data) -> (str, list):
+def clean_data(algo_info, data) -> list:
+    """
+    Clean the data contained in the input file.
+    :param dict algo_info: dict containing all the necessary information to run the program
+    :param list data: output of the open() function from the file
+    :return str: clean_data_int: input data cleaned(cated to int, arranged into lists)
+    """
     raw = data.read()
     newline_split = raw.split('\n')
     uncommented = []
@@ -36,6 +42,11 @@ def clean_data(algo_info, data) -> (str, list):
 
 
 def check_first_line(algo_info, data):
+    """
+    Check the first line of the cleaned data.
+    :param dict algo_info: dict containing all the necessary information to run the program
+    :param list data: list of int created by the clean_data function
+    """
     if len(data) != 1 or data[0] < 2 or type(data[0]) != int:
         algo_info["error"] += "The first line of the file containing the size of the puzzle is not correctly formatted (desired input : single integer (>2) - received input: {0})\n".format(
             data)
@@ -44,6 +55,11 @@ def check_first_line(algo_info, data):
 
 
 def check_lenght(algo_info, data):
+    """
+    Check the format of lines representing the puzzle
+    :param dict algo_info: dict containing all the necessary information to run the program
+    :param list data: list of list of int representing the values of the puzzle to solve
+    """
     size_lst = []
     line_nb = 0
     for each in data:
@@ -61,6 +77,11 @@ def check_lenght(algo_info, data):
 
 
 def check_values(algo_info, data):
+    """
+    Check the values of the lines representing the puzzle
+    :param dict algo_info: dict containing all the necessary information to run the program
+    :param list data: list of list of int representing the values of the puzzle to solve
+    """
     expected_values = [i for i in range(algo_info["board_size"] ** 2)]
     actual_values = []
     for each in data:
@@ -76,9 +97,12 @@ def check_values(algo_info, data):
 
 
 def check_file(algo_info, puzzle_file):
+    """
+    Check every aspects of the input file
+    :param dict algo_info: dict containing all the necessary information to run the program
+    :param str pu: list of list of int representing the values of the puzzle to solve
+    """
     try:
-        if puzzle_file == "/dev/random":
-            raise Exception("WrongFile")
         with open(puzzle_file, 'r') as f:
             data_puzzle = clean_data(algo_info, f)
 
@@ -95,8 +119,6 @@ def check_file(algo_info, puzzle_file):
 
     except IOError:
         algo_info["error"] += "{} cannot be open".format(puzzle_file)
-    except FileNotFoundError:
-        algo_info["error"] += "{} cannot be found".format(puzzle_file)
     except UnboundLocalError:
         algo_info["error"] += "Some data is missing"
     except ValueError:
@@ -104,5 +126,3 @@ def check_file(algo_info, puzzle_file):
     except Exception as e:
         if e.__str__() == "EmptyFile":
             algo_info["error"] += "{0} error: {1} is empty".format(e, puzzle_file)
-        if e.__str__() == "WrongFile":
-            algo_info["error"] += "{0} error: {1} is  not accepted".format(e, puzzle_file)
