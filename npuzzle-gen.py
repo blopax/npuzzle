6fc93e630339942150fcdef3d1bf10278e9009e4
blopax <pablo42clement@gmail.com>
2019-03-19 17:28:45 +0100

    Make n_puzzle generator

--- npuzzle-gen.py
+++ npuzzle-gen.py
@@ -4,44 +4,40 @@ import random
 import utils
 
 
-def make_puzzle(s, solvable):
-    puzzle = [i for i in s ** 2]
-    random.shuffle(puzzle)
+def make_puzzle(size, snail):
+    puzzle_list = [i for i in range(size ** 2)]
+    random.shuffle(puzzle_list)
 
-    snail_goal = utils.puzzle_has_solution(puzzle)
-    if (snail_goal and solvable) or (not snail_goal and not solvable):
-        return puzzle
+    snail_goal = utils.puzzle_has_solution(puzzle_list)
+    if (snail_goal and snail) or (not snail_goal and not snail):
+        return puzzle_list
     else:
-        if puzzle[0] == 0 or puzzle[1] == 0:
-            puzzle[-1], puzzle[-2] = puzzle[-2], puzzle[-1]
+        if puzzle_list[0] == 0 or puzzle_list[1] == 0:
+            puzzle_list[-1], puzzle_list[-2] = puzzle_list[-2], puzzle_list[-1]
         else:
-            puzzle[0], puzzle[1] = puzzle[1], puzzle[0]
-        return puzzle
+            puzzle_list[0], puzzle_list[1] = puzzle_list[1], puzzle_list[0]
+        return puzzle_list
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("size", type=int, help="Size of the puzzle's side. Must be >3.")
-    parser.add_argument("-s", "--solvable", action="store_true", default=False,
-                        help="Forces generation of a solvable puzzle. Overrides -u.")
-    parser.add_argument("-u", "--unsolvable", action="store_true", default=False,
-                        help="Forces generation of an unsolvable puzzle")
+    parser.add_argument("size", type=int, help="Size of the puzzle's side. Must be >3.", default=3)
+    parser.add_argument("-s", "--solvable", type=str, default='True',
+                        help="Says if solvable or not")
     args = parser.parse_args()
     random.seed()
-
-    if args.solvable and args.unsolvable:
-        print("Can't be both solvable AND unsolvable, dummy !")
-        sys.exit(1)
 
     if args.size < 3:
         print("Can't generate a puzzle with size lower than 2. It says so in the help. Dummy.")
         sys.exit(1)
 
-    puzzle = make_puzzle(s, solvable=solv)
+    solvable = args.solvable == 'True'
+    s = args.size
+    puzzle = make_puzzle(s, solvable)
 
     w = len(str(s * s))
-    print("# This puzzle is {}".format("solvable" if solv else "unsolvable"))
+    print("# This puzzle is {}".format("solvable" if solvable else "unsolvable"))
     print("{}\n".format(s))
     for y in range(s):
         for x in range(s):
